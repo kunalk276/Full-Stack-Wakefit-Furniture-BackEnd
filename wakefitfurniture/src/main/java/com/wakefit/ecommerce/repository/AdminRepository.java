399d5d1f452748bb218e9178ac939b5dee01925d
kunal kadam <155727841+kunalk276@users.noreply.github.com>
2024-10-08 18:26:38 +0530

    Delete wakefitfurniture directory

--- wakefitfurniture/src/main/java/com/wakefit/ecommerce/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wakefit.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wakefit.ecommerce.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long> {
-}
--- wakefitfurniture/src/main/java/com/wakefit/ecommerce/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wakefit.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wakefit.ecommerce.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserName(String userName);
-}
